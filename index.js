975756fc1890dc80dbb304598e3acd5bfdcd63ed
Luiz Fernando <luizfernandojunior9999@gmail.com>
2023-04-20 17:01:26 -0300

    App Tasks - Tela de Login

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 //import App from './src/components/App';
-import TaskList from './src/screens/TaskList';
+//import TaskList from './src/screens/TaskList';
+import Auth from './src/screens/Auth';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => TaskList);
+AppRegistry.registerComponent(appName, () => Auth);
