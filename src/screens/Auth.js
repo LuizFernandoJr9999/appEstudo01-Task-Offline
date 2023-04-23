d1548734080660cc21076ca0c6b1a4c8f5e7fb5d
Luiz Fernando <luizfernandojunior9999@gmail.com>
2023-04-23 17:47:50 -0300

    No meio da lição 170

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -6,11 +6,16 @@ import {
   View, 
   TouchableOpacity, 
   Alert
-} from 'react-native'
+} from 'react-native';
+
+import axios from 'axios';
 
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
+
 import AuthInput from '../components/AuthInput';
+
+import {server, showError, showSuccess} from '../common';
 
 export default class Auth extends Component {
   state = {
@@ -23,9 +28,24 @@ export default class Auth extends Component {
 
   signinOrSignup = () => {
     if (this.state.stageNew) {
-      Alert.alert('Sucesso!', 'Criar conta')
+      this.signup();
     } else {
       Alert.alert('Sucesso!', 'Logar')
+    }
+  }
+
+  signup = async () => {
+    try {
+      await axios.post(`${server}/signup`, { 
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password, 
+        confirmPassword: this.state.confirmPassword,
+      })
+      showSuccess('Usuário cadastrado!');
+      this.setState({stageNew: false});
+    } catch (e) {
+      showError(e);
     }
   }
   render() {
--- tasks-backend/api/auth.js
+++ tasks-backend/api/auth.js
@@ -9,8 +9,9 @@ module.exports = app => {
     }
 
     const user = await app.db('users')
-    .where({email: req.body.email})
+    .whereRaw({"LOWER(email) = LOWER(?)", req.body.email})
     .first()
+    
     if (user) {
       bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
         if (err || !isMatch) {
